fd28e71cec7f52388d54142e6ad3fc8d57ecb974
Raqui333 <klebersilva2025@gmail.com>
2024-12-21 03:28:08 -0300

    Test: added new tests for /mine/:address, /login and /addTransaction

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -2,6 +2,11 @@ const request = require('supertest');
 const app = require('../../src/app');
 const RSA = require('node-rsa');
 
+// regex to remove the header and footer of the key pair
+const regex = new RegExp(
+  /\n|-{5}(BEGIN|END) (RSA\s)?(PRIVATE|PUBLIC) KEY-{5}/g
+);
+
 describe('Testing route /createWallet', () => {
   it('Should return an wallet object with 200 status code', async () => {
     const response = await request(app).get('/createWallet').send();
@@ -26,13 +31,9 @@ describe('Testing route /getBalance/:address', () => {
   });
 });
 
-describe('Testing transactions routes', () => {
+describe('Tests for transactions routes', () => {
   const person01 = new RSA({ b: 1024 });
   const person02 = new RSA({ b: 1024 });
-
-  const regex = new RegExp(
-    /\n|-{5}(BEGIN|END) (RSA\s)?(PRIVATE|PUBLIC) KEY-{5}/g
-  );
 
   const person01_address = person01.exportKey('public').replace(regex, '');
   const person02_address = person02.exportKey('public').replace(regex, '');
@@ -82,7 +83,7 @@ describe('Testing transactions routes', () => {
   });
 
   describe('Testing route /addTransaction', () => {
-    it('Should return a message object with 418 status code', async () => {
+    it('Should return a message object with 418 status code for not enough balance', async () => {
       const response = await request(app).post('/addTransaction').send({
         transaction: transaction,
         signature: signature,
@@ -91,6 +92,67 @@ describe('Testing transactions routes', () => {
       expect(response.statusCode).toBe(418);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe('Not enough balance');
+    });
+
+    it('Should return a message object with 418 status code for invalid signature', async () => {
+      const response = await request(app).post('/addTransaction').send({
+        transaction: transaction,
+        signature: 'invalid signature',
+      });
+
+      expect(response.statusCode).toBe(418);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('invalid signature');
+    });
+  });
+});
+
+describe('Testing route /login', () => {
+  const key_pair = new RSA({ b: 1024 });
+
+  const private_key = key_pair.exportKey('private').replace(regex, '');
+  const public_key = key_pair.exportKey('public').replace(regex, '');
+
+  it('Should return a message object with 200 status code for successful login', async () => {
+    const response = await request(app).post('/login').send({
+      private_key: private_key,
+      public_key: public_key,
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toBe('logged');
+  });
+
+  it('Should return a message object with 418 status code for invalid login', async () => {
+    const another_public_key = new RSA({ b: 1024 }).exportKey('public');
+    const response = await request(app).post('/login').send({
+      private_key: private_key,
+      public_key: another_public_key,
+    });
+
+    expect(response.statusCode).toBe(418);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toBe('key pair do not match');
+  });
+
+  describe('Testing route /mine/:address', () => {
+    it('Should return a message object with 418 status code for missing guess_hash', async () => {
+      const response = await request(app).post('/mine/address_string').send();
+
+      expect(response.statusCode).toBe(418);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('Request body mising guess_hash key');
+    });
+
+    it('Should return a message object with 418 status code for wrong guess', async () => {
+      const response = await request(app).post('/mine/address_string').send({
+        guess_hash: 'wrong guess',
+      });
+
+      expect(response.statusCode).toBe(418);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('Wrong guess');
     });
   });
 });
